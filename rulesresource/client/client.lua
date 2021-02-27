6ce8e10a9ea5ceb48907e45978d2cacc4916afcb
Madness <77332699+Jay-Halstead@users.noreply.github.com>
2021-02-27 07:24:41 +0000

    Add files via upload

---
+++ rulesresource/client/client.lua
@@ -0,0 +1,17 @@
+RegisterCommand('rules',function()
+    addClientChat("Do Not Spam Chat")
+    addClientChat("Do Not Harrass Anyone")
+    addClientChat("This is Discord Server to read Full Rules https://discord.gg/NzXUBbSKKB")
+    addClientChat("You Are Not Allowed to Use and Cars That Are Not Your Rank in Any Department")
+    addClientChat("Make Sure You Are Registered in The Cad,https://www.linespolice-cad.com/,Community ID APFEBCW")
+    addClientChat("Make Sure To Subscribe To Acid's Youtube Channel")
+    
+end, false)
+
+function addClientChat(message)
+    TriggerEvent ("chat:addMessage",{
+        color = {0, 255, 255},
+        multiline = true,
+        aregs = {"Server bot"message}
+    })
+end
---
+++ rulesresource/fxmanifest.lua
@@ -0,0 +1,10 @@
+fx_version 'adamant'
+
+game "gta5"
+
+description "This is a rules resource"
+
+client_scripts {
+  "client/client.lua"
+}
+
